be6839afb54f2bb3ae68f2c36b69f9d8bca8b27b
Artem Svetlov <Artem_Svetlov@epam.com>
2018-06-09 10:46:22 +0300

    Added dot net core examples

---
+++ src/NetTypeS/NetTypeS.CoreWebExample/Controllers/ExamplePostController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetTypeS.ExampleData.Models;
+
+namespace NetTypeS.CoreWebExample.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/ExamplePost")]
+    [ApiExplorerSettings(IgnoreApi = false, GroupName = nameof(ExampleGetController))]
+    public class ExamplePostController : Controller
+    {
+        [HttpPost]
+        public ExampleSimpleItem GetExampleSimpleItem()
+        {
+            return new ExampleSimpleItem();
+        }
+
+        [HttpPost]
+        public ExampleSimpleItem GetExampleSimpleItemParameters(int id)
+        {
+            return new ExampleSimpleItem();
+        }
+
+        [HttpPost]
+        public ExampleComplexItem GetExampleComplexItem()
+        {
+            return new ExampleComplexItem();
+        }
+
+        [HttpPost]
+        public ExampleComplexItem GetExampleComplexItemParameter(int id)
+        {
+            return new ExampleComplexItem();
+        }
+
+        [HttpPost]
+        public ExampleGenericItem<ExampleComplexItem> GetExampleGenericItem()
+        {
+            return new ExampleGenericItem<ExampleComplexItem>();
+        }
+    }
+}
