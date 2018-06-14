58c32ad1090aa3e6defa9e0f27d5ce68e7b12596
Artem Svetlov <Artem_Svetlov@epam.com>
2018-06-14 08:20:55 +0300

    Revert console generator

---
+++ src/NetTypeS/NetTypeS.Console/ConsoleGenerator.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web.Http;
+using System.Web.Http.Description;
+using NetTypeS.WebApi.Framework;
+
+namespace NetTypeS.Console
+{
+    internal class ConsoleGenerator
+    {
+        private readonly string _sourceAssemblyPath;
+        private readonly string _outputFolder;
+        private readonly string[] _exclude;
+
+        public ConsoleGenerator(string sourceAssemblyPath, string outputFolder, string[] exclude)
+        {
+            _sourceAssemblyPath = sourceAssemblyPath;
+            _outputFolder = outputFolder;
+            _exclude = exclude ?? new string[0];
+        }
+
+        public void UpdateFile(string fileName, string newContent)
+        {
+            string path = Path.Combine(_outputFolder, fileName);
+
+            if (!Directory.Exists(_outputFolder))
+            {
+                Directory.CreateDirectory(_outputFolder);
+            }
+
+            if (!File.Exists(path) || File.ReadAllText(path) != newContent)
+            {
+                File.WriteAllText(path, newContent);
+            }
+        }
+
+        public void GenerateAll()
+        {
+            var httpConfig = new HttpConfiguration();
+
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.AssemblyResolve += LoadFromTargetFolder;
+            Assembly assembly = Assembly.LoadFrom(_sourceAssemblyPath);
+            var initializerClass = assembly.GetTypes().Single(t => t.Name == "WebApiConfig");
+            initializerClass.GetMethod("Register")?.Invoke(null, new object[] { httpConfig });
+
+            httpConfig.EnsureInitialized();
+
+            var explorer = httpConfig.Services.GetApiExplorer();
+            var generator = new WebApiFrameworkGenerator(
+                apiFilter: IsSuitableApi
+            );
+            var files = generator.GenerateAll(explorer);
+
+            UpdateFile("models.ts", files.Models);
+            UpdateFile("apiDefinition.ts", files.Api);
+        }
+
+        private bool IsSuitableApi(ApiDescription api)
+        {
+            return !_exclude.Contains(api.ActionDescriptor.ControllerDescriptor.ControllerName);
+        }
+
+        private Assembly LoadFromTargetFolder(object sender, ResolveEventArgs args)
+        {
+            //var asm = Assembly.Load(args.Name);
+
+            var name = new AssemblyName(args.Name).Name;
+
+            if (name == "System.Web.Cors")
+            {
+                name = "System.Web.Http.Cors";
+            }
+
+            var folderPath = Path.GetDirectoryName(_sourceAssemblyPath);
+            var assemblyPath = Path.Combine(folderPath, name + ".dll");
+            if (!File.Exists(assemblyPath)) return null;
+            var assembly = Assembly.LoadFrom(assemblyPath);
+            return assembly;
+        }
+    }
+}
---
+++ src/NetTypeS/NetTypeS.Console/Program.cs
@@ -0,0 +1,33 @@
+﻿using CLAP;
+
+namespace NetTypeS.Console
+{
+    internal class Commands
+    {
+        [Empty, Help]
+        public static void Help(string help)
+        {
+            System.Console.WriteLine(help);
+        }
+
+        [Verb(Description = "Generate TS interfaces and API definition files.")]
+        public static void GenTS(
+            [System.ComponentModel.Description("Path to assembly containing WebApi classes.")] string assemblyPath,
+            [System.ComponentModel.Description("Output folder for TS files.")] string outputFolder,
+            [System.ComponentModel.Description("Exclude controller from TS generation")] string[] exclude
+        )
+        {
+            var gen = new ConsoleGenerator(assemblyPath, outputFolder, exclude);
+            gen.GenerateAll();
+        }
+    }
+
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Parser.RunConsole<Commands>(args);
+        }
+    }
+}
